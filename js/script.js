349471365af80d797ab2adc6708a43b7b2554b20
Ibrohiym <ibaxodirow@gmail.com>
2021-11-23 15:17:43 +0500

    polniy ozgardi

--- js/script.js
+++ js/script.js
@@ -18,3 +18,12 @@ window.onload = function () {
         .bindPopup('This is Manchester!')
         .addTo(map);
 }
+
+
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
